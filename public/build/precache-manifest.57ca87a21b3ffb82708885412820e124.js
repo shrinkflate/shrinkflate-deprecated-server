b856dffe9993ec12c9455ad3d6a1738274e19b0a
Rocky <wpkpda@gmail.com>
2020-08-28 22:59:37 +0600

    build

---
+++ public/build/precache-manifest.57ca87a21b3ffb82708885412820e124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5573072bfe68084a37ad2ce7e83aea32",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "ff7277fab733bbdeea0a",
+    "url": "/frontend/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ff7277fab733bbdeea0a",
+    "url": "/frontend/static/js/2.68abe4e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend/static/js/2.68abe4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c690721d2d2166d48311",
+    "url": "/frontend/static/js/main.3a5059e3.chunk.js"
+  },
+  {
+    "revision": "9a1fd2a57778b89dafcf",
+    "url": "/frontend/static/js/runtime-main.14c5bd34.js"
+  }
+]);
